8dc3d286e9914df865307ee7c2f583063bbf7b7f
Jaffer's Tech-Hub <101195584+Jaffer74@users.noreply.github.com>
2024-12-26 21:58:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 
 const cors = require('cors');
 app.use(cors());
