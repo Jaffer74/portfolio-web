65297b6435e51736e67b5384ee991fa4f7a9419e
Jaffer's Tech-Hub <101195584+Jaffer74@users.noreply.github.com>
2024-12-28 00:38:14 +0530

    Update Header.js

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
                     <img
                       width={228}
                       height={38}
-                      src="assets/imagesjafferdev.jpg"
+                      src="assets/images/jafferdev.jpg"
                       alt=""
                     />
                   </a>
